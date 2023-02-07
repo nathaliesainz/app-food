e284138ee1028cb433c28d56665fb56370f783b8
nathaliesainz <nathaliesainz13@gmail.com>
2023-02-06 22:43:01 -0600

    Corrected hook, added message for error

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -4,18 +4,27 @@ import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
 
 const SearchScreen = () => {
-  const [term,setTerm] = useState('');
+  const [term, setTerm] = useState('');
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const searchApi = async () => {
-    const response = await yelp.get('/search', {
-      params: {
-        limit: 50,
-        term,
-        location: 'san jose'
-      }
-    });
-    setResults(response.data.businesses);
+
+  //   if (errorMessage) {
+  //     setErrorMessage('');
+  // }
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term,
+          location: 'san jose'
+        }
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage('Something went wrong');
+    }
   };
 
   return (
@@ -25,7 +34,7 @@ const SearchScreen = () => {
         onTermChange={setTerm}
         onTermSubmit={searchApi}
       />
-      <Text>Search Screen</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
   );
